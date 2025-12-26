6cc5a99ba8ebc905cc4455b619e5837ee03b73a5
PavanPawar12 <pavanpawar9923100@gmail.com>
2025-12-26 23:10:09 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,18 @@ import About from "./components/About";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-
 function App() {
 
   return (
     <>
-      
       <Navbar/>
-
       <Hero/>
       <About/>
       <Project/>
       <Contact/>
       <Footer/> 
+
+      
     </>
   )
 }
